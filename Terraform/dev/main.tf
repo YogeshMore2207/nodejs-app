3fd21c4915aa3feb0e97b1e593cba85cbd12b12a
YogeshMore2207 <102284703+YogeshMore2207@users.noreply.github.com>
2024-05-20 17:04:51 +0530

    Update main.tf

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 module "hello_world-node-app" {
-  source = "./Terraform/modules"
+  source = ".../modules"
   docker_image = "https://hub.docker.com/repository/docker/revoltrv/hello-world-nodejs"
   subnets = ["subnet-00cb93d5fce930350", "subnet-02f76097d90c7a6ad", "subnet-01fcaf3bb11016e82"]
   vpc_id = "vpc-041cad60f8ff97492"
